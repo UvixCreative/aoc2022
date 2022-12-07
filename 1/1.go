a37a301deeab5cde0826a8cc7bfbf6f012f1ce40
Ben <benrankin4000@gmail.com>
2022-12-06 21:28:21 -0500

    Finished day 1 problem 1

--- 1/1.go
+++ 1/1.go
@@ -2,14 +2,15 @@ package main
 
 import "os"
 import "fmt"
-import "reflect"
 import "strings"
 import "strconv"
 
 func main() {
 	input, err := os.ReadFile("input.txt")
 	lines := strings.Split(string(input), "\n")
-	var elve_cals [5]int
+	var elve_cals []int
+	var tmp_total int
+	i := 0
 
 	if err != nil {
 		panic(err)
@@ -17,12 +18,24 @@ func main() {
 
 	for _, s := range lines {
 		if (s == "") {
-			fmt.Println("EMPTY")
+			fmt.Printf("Elf %d: %d calories\n", i, tmp_total)
+			elve_cals = append(elve_cals, tmp_total)
+			tmp_total = 0
+			i++
 		} else {
-			fmt.Println(s)
-			fmt.Println(strconv.Atoi(s))
+			val, _ := strconv.Atoi(s)
+			tmp_total += val
 		}
 	}
-	// fmt.Print(lines)
-	fmt.Printf("%s", reflect.TypeOf(lines))
+
+	var biggest int
+	var biggestIdx int
+	for index, val := range elve_cals {
+		if (val > biggest) {
+			biggest = val
+			biggestIdx = index
+		}
+	}
+
+	fmt.Printf("\nElf %d has the greatest, at %d calories\n", biggestIdx, biggest)
 }
